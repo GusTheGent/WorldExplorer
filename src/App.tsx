b36b83e7fdb89b3e23d2958e18e944acf0e9ccd2
angelisk_dev <gusangelis90@gmail.com>
2023-08-30 12:21:46 +0300

    update (nav links)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import Navbar from "components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "pages/Home/Home";
-import "./App.scss";
 import CountryDetails from "pages/CountryDetails/CountryDetails";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/WorldExplorer" element={<Home />} />
         <Route path="/details/:countryName" element={<CountryDetails />} />
       </Routes>
     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
 import { NavbarProps } from "./types";
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FunctionComponent<NavbarProps> = () => {
-  const navigate = useNavigate();
-  const handleOnClick = () => navigate('/');
 
-  
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{ backgroundColor: "#333" }}>
           <Toolbar>
             <Typography variant="h4" component="div">
-              <Link to="/" style={{ textDecoration: "none", color: "white" }} onClick={handleOnClick}>
+              <Link to="/WorldExplorer" style={{ textDecoration: "none", color: "white" }}>
                 World Explorer
               </Link>
             </Typography>
